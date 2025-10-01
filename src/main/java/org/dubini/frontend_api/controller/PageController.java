6e17946ac4d24c475a3df23db752fa1232bfab38
Francisco <hernandezcrosafrancisco@gmail.com>
2025-10-01 13:40:53 +0100

    implemented basic backend, added several pages

---
+++ src/main/java/org/dubini/frontend_api/controller/PageController.java
@@ -0,0 +1,46 @@
+package org.dubini.frontend_api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+
+    @GetMapping("/documentos-institucionales")
+    public String documents() {
+        return "documents";
+    }
+
+    @GetMapping("/contacto")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/alexandra-dubini")
+    public String alexandra() {
+        return "alexandra";
+    }
+
+    @GetMapping("/privacidad")
+    public String privacy() {
+        return "privacy";
+    }
+
+    @GetMapping("/terminos-y-condiciones")
+    public String terms() {
+        return "terms";
+    }
+
+    @GetMapping("/junta-directiva")
+    public String directorate() {
+        return "directorate";
+    }
+    
+    
+}
