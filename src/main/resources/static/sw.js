19ad5aab8f6c4496e5d8f7f5e0e32058bf62c934
Francisco Airam <hernandezcrosafrancisco@gmail.com>
2025-11-25 23:58:42 +0000

    Update sw.js

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(self.location.search);
-const VERSION = 'v7.7.9';
+const VERSION = 'v8.7.9';
 const CACHE_NAME = `dubini-static-cache-${VERSION}`;
 
 self.addEventListener('install', event => {
